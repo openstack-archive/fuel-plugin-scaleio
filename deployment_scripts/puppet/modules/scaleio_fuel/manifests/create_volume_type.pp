b810b87f186cf3ee8d8f661a2818de547fe713b0
Adrian Moreno <adrian@morenomartinez.com>
2015-12-09 18:15:43 +0100

    Fixed provisioning type key. All volumes should be created with thin provisioning
    Change-Id: I897a303bd0e0d093695583bf5588df29d1e87afa

--- deployment_scripts/puppet/modules/scaleio_fuel/manifests/create_volume_type.pp
+++ deployment_scripts/puppet/modules/scaleio_fuel/manifests/create_volume_type.pp
@@ -13,7 +13,7 @@ inherits scaleio_fuel::params {
   } ->
 
   exec { "Create Cinder volume type extra specs for \'${volume_type}\'":
-    command => "bash -c 'source /root/openrc; cinder type-key ${volume_type} set sio:pd_name=${protection_domain} sio:provisioning_type=thin sio:sp_name=${storage_pool}'",
+    command => "bash -c 'source /root/openrc; cinder type-key ${volume_type} set sio:pd_name=${protection_domain} sio:provisioning=thin sio:sp_name=${storage_pool}'",
     path    => ['/usr/bin', '/bin'],
     onlyif  => "bash -c 'source /root/openrc; cinder type-list |grep -q \" ${volume_type} \"'",
   }
