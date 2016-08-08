84796d4c47dd0195fa73571748c0e5cf47b1d168
alexey-mr <alexey.morlang@gmail.com>
2016-08-08 18:09:43 +0300

    Glance over Cinder support
    Change-Id: Ib38a8bd3e5c02ab9214726ab313024fda46cd754

---
+++ deployment_scripts/puppet/manifests/glance_create_test_image.pp
@@ -0,0 +1,16 @@
+# The puppet configures OpenStack glance to use ScaleIO via Cinder.
+
+$scaleio = hiera('scaleio')
+if $scaleio['metadata']['enabled'] {
+  $all_nodes = hiera('nodes')
+  $nodes = filter_nodes($all_nodes, 'name', $::hostname)
+  if empty(filter_nodes($nodes, 'role', 'primary-controller')) {
+    fail("create test image task should be run only on primary-controller, but node ${::hostname} is not primary-controller")
+  }
+  if $scaleio['use_scaleio_for_glance'] {
+    exec {"test image create":
+      command => "ruby /etc/puppet/modules/osnailyfacter/modular/astute/upload_cirros.rb",
+      path    => '/bin:/usr/bin:/usr/local/bin'
+    }
+  }
+}
--- deployment_scripts/puppet/manifests/sdc_server.pp
+++ deployment_scripts/puppet/manifests/sdc_server.pp
@@ -2,8 +2,18 @@
 
 $scaleio = hiera('scaleio')
 if $scaleio['metadata']['enabled'] {
-  class {'::scaleio::sdc_server':
-    ensure => 'present',
-    mdm_ip => undef,
+  $all_nodes = hiera('nodes')
+  $nodes = filter_nodes($all_nodes, 'name', $::hostname)
+  $is_compute = !empty(filter_nodes($nodes, 'role', 'compute'))
+  $is_cinder = !empty(filter_nodes($nodes, 'role', 'cinder'))
+  $is_glance = (!empty(filter_nodes($nodes, 'role', 'primary-controller')) or !empty(filter_nodes($nodes, 'role', 'controller'))) and $scaleio['use_scaleio_for_glance']
+  $need_sdc = $is_compute or $is_cinder or $is_glance
+  if $need_sdc {
+    class {'::scaleio::sdc_server':
+      ensure => 'present',
+      mdm_ip => undef,
+    }
+  } else{
+      notify {"Skip SDC server task on the node ${::hostname}": }
   }
 }
